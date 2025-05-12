040dd725108edee299a34884b2290543ea678798
daniraheim <daniraheim@gmail.com>
2025-05-12 18:48:03 -0500

    fixed user file

--- users.js
+++ users.js
@@ -4,31 +4,39 @@ const userModel = require('./userModel.js');
 const db = require('./dbUtil.js');
 
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+
     console.log(req.body)
     const { username, password } = req.body;
 
+
+
   console.log('a')
     const query = 'SELECT * FROM users WHERE username = ? and password = ?';
-  
-    db.query(query, [username, password], (error, results) => {
-      console.log(error)
-      if (error) {
-        console.error('Database query error:', error);
-        return res.status(500).json({ success: false, message: 'Server error' });
-      }
-      if (results.length === 0) {
+    const result = await userModel.getAllUsers(username, password);
+    console.log(result[0]);
+    // db.query(query, [username, password], (error, results) => {
+    //   console.log(error)
+    //   if (error) {
+    //     console.error('Database query error:', error);
+    //     return res.status(500).json({ success: false, message: 'Server error' });
+    //   }
+    //   if (results.length === 0) {
+    //     return res.status(400).json({ success: false, message: 'User not found' });
+    //   }
+ 
+      // const passwordsMatch = user.password === password;
+ 
+      if (result[0].length == 0) {
         return res.status(400).json({ success: false, message: 'User not found' });
       }
-      const user = results[0];
-  
-      const passwordsMatch = user.password === password;
-  
-      if (!passwordsMatch) {
-        return res.status(400).json({ success: false, message: 'Incorrect password' });
+      else
+      {
+
+      const user = result[0];
+        return res.status(200).json({ success: true, message: 'Login successful', user: { username: user.username } });
       }
-      return res.status(200).json({ success: true, message: 'Login successful', user: { username: user.username } });
-    });
+    //});
   });
 
 router.post('/register', async (request, response) => {
@@ -42,7 +50,5 @@ router.post('/register', async (request, response) => {
         return response.json({message: "User Added"});
     }
 })
-
-
 
 module.exports = router;
